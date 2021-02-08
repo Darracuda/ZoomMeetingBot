0bf33ad2471eaecbe9a369ec9c5d4172ab24551c
darracuda <diane.kolpakova@gmail.com>
2021-02-08 13:43:50 +0300

    added loop for restarting the app

--- src/main/kotlin/org/example/main/Main.kt
+++ src/main/kotlin/org/example/main/Main.kt
@@ -26,62 +26,64 @@ class Main {
             val zoomPassword = settings.zoom.password
             val zoomToken = settings.zoom.token
 
-            val mailboxManager = MailboxManager(settings)
+            while(true) {
+                val mailboxManager = MailboxManager(settings)
 
-            val icsMeetingManager = IcsMeetingManager(mailboxManager)
-            logger.info("Starting meetings download")
-            val icsMeetings = icsMeetingManager.getMeetingsFromMailbox()
-            logger.info("Meeting download complete")
+                val icsMeetingManager = IcsMeetingManager(mailboxManager)
+                logger.info("Starting meetings download")
+                val icsMeetings = icsMeetingManager.getMeetingsFromMailbox()
+                logger.info("Meeting download complete")
 
-            for(icsMeeting in icsMeetings){
-                val apiInstance = MeetingsApi()
-                val meetingSettings = CreateMeetingRequest.MeetingSettings(
-                    host_video = settings.zoomMeeting.hostVideo,
-                    participant_video = settings.zoomMeeting.participantVideo,
-                    cn_meeting = settings.zoomMeeting.cnMeeting,
-                    in_meeting = settings.zoomMeeting.inMeeting,
-                    join_before_host = settings.zoomMeeting.joinBeforeHost,
-                    mute_upon_entry = settings.zoomMeeting.muteUponEntry,
-                    watermark = settings.zoomMeeting.watermark,
-                    use_pmi = settings.zoomMeeting.usePmi,
-                    approval_type = settings.zoomMeeting.approvalType,
-                    audio = settings.zoomMeeting.audio,
-                    auto_recording = settings.zoomMeeting.autoRecording,
-                )
-                val request = CreateMeetingRequest(
-                    topic = icsMeeting.subject,
-                    agenda = icsMeeting.description,
-                    type = CreateMeetingRequest.MeetingType.ScheduledMeeting,
-                    start_time = icsMeeting.startTime,
-                    duration = icsMeeting.duration.toMinutes(),
-                    password = zoomPassword,
-                    settings = meetingSettings,
-                )
-                try {
-                    val response = apiInstance.createMeeting(zoomToken, zoomLogin, request)
-                    logger.info("Meeting created")
-                    logger.info("start time: ${toLocal(response.start_time)}")
-                    logger.info("Response: $response")
-
-                    val zoomMeeting = ZoomMeeting(
-                        icsMeeting.subject,
-                        icsMeeting.description,
-                        response.start_time,
-                        icsMeeting.duration,
-                        response.join_url,
-                        icsMeeting.attendees,
+                for (icsMeeting in icsMeetings) {
+                    val apiInstance = MeetingsApi()
+                    val meetingSettings = CreateMeetingRequest.MeetingSettings(
+                        host_video = settings.zoomMeeting.hostVideo,
+                        participant_video = settings.zoomMeeting.participantVideo,
+                        cn_meeting = settings.zoomMeeting.cnMeeting,
+                        in_meeting = settings.zoomMeeting.inMeeting,
+                        join_before_host = settings.zoomMeeting.joinBeforeHost,
+                        mute_upon_entry = settings.zoomMeeting.muteUponEntry,
+                        watermark = settings.zoomMeeting.watermark,
+                        use_pmi = settings.zoomMeeting.usePmi,
+                        approval_type = settings.zoomMeeting.approvalType,
+                        audio = settings.zoomMeeting.audio,
+                        auto_recording = settings.zoomMeeting.autoRecording,
                     )
+                    val request = CreateMeetingRequest(
+                        topic = icsMeeting.subject,
+                        agenda = icsMeeting.description,
+                        type = CreateMeetingRequest.MeetingType.ScheduledMeeting,
+                        start_time = icsMeeting.startTime,
+                        duration = icsMeeting.duration.toMinutes(),
+                        password = zoomPassword,
+                        settings = meetingSettings,
+                    )
+                    try {
+                        val response = apiInstance.createMeeting(zoomToken, zoomLogin, request)
+                        logger.info("Meeting created")
+                        logger.info("start time: ${toLocal(response.start_time)}")
+                        logger.info("Response: $response")
 
-                    mailboxManager.sendMessage(zoomMeeting)
-                } catch (e: ClientException) {
-                    e.printStackTrace()
-                    logger.error("4xx response calling MeetingsApi#meetingCreate")
-                } catch (e: ServerException) {
-                    e.printStackTrace()
-                    logger.error("5xx response calling MeetingsApi#meetingCreate")
-                } catch (e: Exception) {
-                    logger.error("Exception: $e")
-                    e.printStackTrace()
+                        val zoomMeeting = ZoomMeeting(
+                            icsMeeting.subject,
+                            icsMeeting.description,
+                            response.start_time,
+                            icsMeeting.duration,
+                            response.join_url,
+                            icsMeeting.attendees,
+                        )
+
+                        mailboxManager.sendMessage(zoomMeeting)
+                    } catch (e: ClientException) {
+                        e.printStackTrace()
+                        logger.error("4xx response calling MeetingsApi#meetingCreate")
+                    } catch (e: ServerException) {
+                        e.printStackTrace()
+                        logger.error("5xx response calling MeetingsApi#meetingCreate")
+                    } catch (e: Exception) {
+                        logger.error("Exception: $e")
+                        e.printStackTrace()
+                    }
                 }
             }
         }
